430c1d53f251533aa4b841fcdebc3f173b79d2ba
Guilherme Setim <64696390+guilhermesetim@users.noreply.github.com>
2021-11-08 11:17:29 -0300

    instalação
    instalação das ferramentas de teste do composer php

---
+++ install-tools-composer.sh
@@ -0,0 +1,7 @@
+## navegar até o diretório do projeto
+
+composer require --dev phpunit/phpunit
+composer require --dev squizlabs/php_codesniffer
+composer require --dev phan/phan
+
+echo "Ferramentas do composer instaladas com sucesso!"
---
+++ scripts.md
@@ -0,0 +1,10 @@
+## Acrescentar no arquivo composer.json
+
+```
+"scripts": {
+    "test": "phpunit test[Arquivo que ocorrerá o teste].php,
+    "cs": "phpcs --standard=PS12 src/",
+    "phan": "phan --allow-polyfill-parser"
+}
+
+```
